f384dcb3a8b0052d7c9e89fa9dcda6275f16d582
robert.kruger <bradgp84@gmail.com>
2021-06-14 09:06:41 -0400

    changes to confis.js and jobly.sql files

--- jobly.sql
+++ jobly.sql
@@ -1,3 +1,5 @@
+\echo 'Delete and recreate jobly db?'
+\prompt 'Return for yes or control-C to cancel > ' foo
 
 DROP DATABASE jobly;
 CREATE DATABASE jobly;
@@ -6,6 +8,8 @@ CREATE DATABASE jobly;
 \i jobly-schema.sql
 \i jobly-seed.sql
 
+\echo 'Delete and recreate jobly_test db?'
+\prompt 'Return for yes or control-C to cancel > ' foo
 
 DROP DATABASE jobly_test;
 CREATE DATABASE jobly_test;
