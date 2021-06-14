ea25b56ac244553af1459408a3a58dbb47e3b69a
robert.kruger <bradgp84@gmail.com>
2021-06-13 20:47:53 -0400

    d

--- jobly.sql
+++ jobly.sql
@@ -1,5 +1,3 @@
-\echo 'Delete and recreate jobly db?'
-\prompt 'Return for yes or control-C to cancel > ' foo
 
 DROP DATABASE jobly;
 CREATE DATABASE jobly;
@@ -8,8 +6,6 @@ CREATE DATABASE jobly;
 \i jobly-schema.sql
 \i jobly-seed.sql
 
-\echo 'Delete and recreate jobly_test db?'
-\prompt 'Return for yes or control-C to cancel > ' foo
 
 DROP DATABASE jobly_test;
 CREATE DATABASE jobly_test;
